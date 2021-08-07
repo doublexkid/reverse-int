a08318ffed5626f8055723951ee542538d67d113
Anna Mugu <86118286+doublexkid@users.noreply.github.com>
2021-08-08 02:34:18 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+    let reversed = 0;
+    while (n) {
+        reversed = reversed * 10 + n % 10;
+        n = Math.floor(n / 10);
+    }
+    return reversed;
   
 }
+  
