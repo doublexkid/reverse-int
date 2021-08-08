cba79e0ca40bd3d54b86b6057356174e9099c564
Anna Mugu <86118286+doublexkid@users.noreply.github.com>
2021-08-08 03:41:29 +0300

    feat: edit solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 module.exports = function reverse (n) {
-    let reversed = 0;
-    while (n) {
-        reversed = reversed * 10 + n % 10;
-        n = Math.floor(n / 10);
-    }
-    return reversed;
+    return parseInt(n.toString().split("").reverse().join(""));
   
 }
   
